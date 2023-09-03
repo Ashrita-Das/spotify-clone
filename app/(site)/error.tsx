0eaba600cf1b236ca486cea81273288adc3acb2e
Ashrita-Das <ashrita315@gmail.com>
2023-09-03 12:32:34 +0530

    final project

---
+++ app/(site)/error.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Box from "@/components/Box";
+
+const Error = () => {
+    return (
+        <Box className="h-full flex items-center justify-center">
+            <div className="text-nutral-400">
+                Uh-oh, something went wrong.
+            </div>
+        </Box>
+    )
+}
+
+export default Error
